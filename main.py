4010dd8049f54ead2408aeb2ecdcd6fb20183fad
SnehaReddy105 <119911379+SnehaReddy105@users.noreply.github.com>
2023-03-17 14:59:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,8 +39,8 @@ if rad=="Sentiment Analysis":
     
     
         pre = st.text_input('Clean Text: ')
-        if pre:
-            st.write(cleantext.clean(pre, clean_all= False, extra_spaces=True ,stopwords=True ,lowercase=True ,numbers=True , punct=True))
+        #if pre:
+            #st.write(cleantext.clean(pre, clean_all= False, extra_spaces=True ,stopwords=True ,lowercase=True ,numbers=True , punct=True))
     
     with st.expander('Analyze CSV'):
         upl = st.file_uploader('Upload file')
